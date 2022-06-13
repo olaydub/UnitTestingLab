302385d66b47d682d14b8cfc651978506028d388
Oliver Waller <o.waller@bellevuecollege.edu>
2022-06-13 09:49:09 -0700

    added implementation for the detectCapitalUse method

--- src/main/java/CILab.java
+++ src/main/java/CILab.java
@@ -13,7 +13,36 @@ public class CILab implements CILabInterface {
 
     @Override
     public boolean detectCapitalUse() {
-        return false;
+        //null check
+        if (myString.length() > 1){
+            //check first character, if upper case ensure the rest are only upper case or lower case (exclusive)
+            if (Character.isUpperCase(myString.charAt(0))){
+                //they should all be upper case now
+                if (Character.isUpperCase(myString.charAt(1))){
+                    //ensure the rest are upper case
+                    for (int i = 2; i < myString.length(); i++){
+                        if (Character.isLowerCase(myString.charAt(i))){
+                            return false;
+                        }
+                    }
+                } else { //remaining should be lower case
+                    for (int i = 2; i < myString.length(); i++){
+                        if (Character.isUpperCase(myString.charAt(i))){
+                            return false;
+                        }
+                    }
+                }
+            } else { //they should all be lower case
+                for (int i = 1; i < myString.length(); i++){
+                    if (Character.isUpperCase(myString.charAt(i))){
+                        return false;
+                    }
+                }
+            }
+
+        }
+        return true; //string length 1 or they it satisfies all conditions
+
     }
 
 }
